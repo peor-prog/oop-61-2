8497a92ff8fc1cf2f86e5794d77b3f9d416b386c
peor-prog <kirillsolonskij26@gmail.com>
2025-12-24 17:34:24 +0600

    hw6.py

---
+++ hw6.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+print(Fore.RED + "Это красный текст")
+print(Fore.GREEN + "Это зелёный текст")
+print(Fore.BLUE + "Это синий текст")
+print(Style.BRIGHT + "Это яркий текст")
+
+
+#эта библиотека предназначена для перекрашивания цвета в консоли
